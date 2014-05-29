b4df791263b5540e4416965a6b409cecd3e13ea1
Luke Jolly <lukejamesjolly@gmail.com>
2014-05-28 21:08:03 -0400

    Added the reset of the fields to History
    Added the reset of the fields to History

--- history.go
+++ history.go
@@ -4,10 +4,15 @@ import "github.com/AlekSi/reflector"
 
 // https://www.zabbix.com/documentation/2.0/manual/appendix/api/history/definitions
 type History struct {
-	ItemId      string    `json:"itemid,omitempty"`
-	Clock       int       `json:"clock"`
-	Value	    string    `json:"value"`
-	Nanoseconds int       `json:"ns,omitempty"`
+	ItemId      string `json:"itemid,omitempty"`
+	Clock       int    `json:"clock"`
+	Value       string `json:"value"`
+	Nanoseconds int    `json:"ns,omitempty"`
+	Id          string `json:"id,omitempty"`
+	LogEventId  int    `json:"logeventid,omitempty"`
+	Severity    int    `json:"severity,omitempty"`
+	Source      string `json:"source,omitempty"`
+	TimeStamp   int    `json:"timestamp,omitempty"`
 }
 
 // Wrapper for item.get https://www.zabbix.com/documentation/2.0/manual/appendix/api/item/get
@@ -23,4 +28,3 @@ func (api *API) HistoriesGet(params Params) (res []History, err error) {
 	reflector.MapsToStructs2(response.Result.([]interface{}), &res, reflector.Strconv, "json")
 	return
 }
-
